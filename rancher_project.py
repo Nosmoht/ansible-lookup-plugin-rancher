9a52526749d74d6d1e789ec34976ad97f84d628b
Thomas Krahn <ntbc@gmx.net>
2017-06-19 12:00:41 +0200

    rancher_project: Use environment variable RANCHER_URL, RANCHER_ACCESS_KEY and RANCHER_SECRET_KEY as defaults for url, url_username and url_password

--- rancher_project.py
+++ rancher_project.py
@@ -11,20 +11,30 @@ except ImportError:
 
     display = Display()
 
-ANSIBLE_RACHER_URL = 'http://127.0.0.1:8080'
+RANCHER_API = 'v1'
+RANCHER_URL = os.environ.get('RANCHER_URL', 'http://127.0.0.1:8080')
+RANCHER_ACCESS_KEY = os.environ.get('RANCHER_ACCESS_KEY', None)
+RANCHER_SECRET_KEY = os.environ.get('RANCHER_SECRET_KEY', None)
 
 
 class LookupModule(LookupBase):
-    def run(self, terms, variables=None, **kwargs):
+    def run(self, terms, **kwargs):
         validate_certs = kwargs.get('validate_certs', True)
         ret = []
-        url_base = kwargs.pop('url', ANSIBLE_RACHER_URL)
+        api = kwargs.pop('api', ANSIBLE_API)
+        url = kwargs.pop('url', RANCHER_URL)
+        url_username = kwargs.pop('url_username', RANCHER_ACCESS_KEY)
+        url_password = kwargs.pop('url_password', RANCHER_SECRET_KEY)
+        validate_certs = kwargs.get('validate_certs', True)
 
         for term in terms:
-            url = '%s/v1/projects' % (url_base)
+            url = '%s/%s/projects' % (url, api)
             display.vvvv('rancher_project lookup of %s on %s' % (term, url))
             try:
-                response = open_url(url)
+                response = open_url(url=url,
+                                    url_username=url_username,
+                                    url_password=url_password,
+                                    validate_certs=validate_certs)
             except HTTPError as e:
                 raise AnsibleError("Received HTTP error for %s : %s" % (term, str(e)))
             except URLError as e:
@@ -34,9 +44,13 @@ class LookupModule(LookupBase):
             except ConnectionError as e:
                 raise AnsibleError("Error connecting to %s: %s" % (term, str(e)))
 
-            data = json.loads(response.read())
-            for item in data.get('data'):
-                if item.get('name') == term and item.get('type') == 'project':
-                    ret.append(item)
+            body = json.loads(response.read())
+            data = body.get('data')
+            if terms:
+                for item in data:
+                    if item.get('name') == term and item.get('type') == 'project':
+                        ret.append(item)
+            else:
+                ret.append(data)
 
         return ret
